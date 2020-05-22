3d2f2a04604777685bf8652d69f5826f6ae01d3a
kaelif <flanagan.kaeli@gmail.com>
2020-05-21 18:38:47 -0700

    added movement and borders

--- src/brickBreaker/Gameplay.java
+++ src/brickBreaker/Gameplay.java
@@ -58,16 +58,34 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
     }
 
     @Override
-    public void keyTyped(KeyEvent e) {
+    public void keyTyped(KeyEvent e) {}
 
-    }
     @Override
     public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (playerX >= 600) {
+                playerX = 600;
+            } else {
+                moveRight();
+            }
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (playerX < 10) {
+                playerX = 10;
+            } else {
+                moveLeft();
+            }
+        }
+    }
+    public void moveRight() {
+        play = true;
+        playerX += 20;
+    }
 
+    public void moveLeft() {
+        play = true;
+        playerX -= 20;
     }
 
     @Override
-    public void keyReleased(KeyEvent e) {
-
-    }
+    public void keyReleased(KeyEvent e) {}
 }
