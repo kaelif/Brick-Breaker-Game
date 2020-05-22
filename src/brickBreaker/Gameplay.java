2cd1aabeeaed2c0ec31d1e935b9bced1d35fd2ef
kaelif <flanagan.kaeli@gmail.com>
2020-05-21 18:30:39 -0700

    added ball and paddle

---
+++ src/brickBreaker/Gameplay.java
@@ -0,0 +1,73 @@
+package brickBreaker;
+
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.*;
+import java.util.Timer;
+
+
+public class Gameplay extends JPanel implements KeyListener, ActionListener{
+    private boolean play = false;
+    private int score = 0;
+
+    private int totalBricks = 21;
+
+    private Timer timer;
+    private int delay = 8;
+
+    private int playerX = 310;
+
+    private int ballPosX = 120;
+    private int ballPosY = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
+
+    public Gameplay() {
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(delay, this);
+        timer.start();
+    }
+
+    public void paint(Graphics g) {
+        // Background
+        g.setColor(Color.black);
+        g.fillRect(1, 1, 692, 592);
+
+        // Borders
+        g.setColor(Color.yellow);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691,  0, 3, 592);
+
+        // The paddle
+        g.setColor(Color.green);
+        g.fillRect(playerX, 550, 100, 8);
+
+        // The ball
+        g.setColor(Color.yellow);
+        g.fillRect(ballPosX, ballPosY, 20, 20);
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
